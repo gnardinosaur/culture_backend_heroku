86b9132a028545ea1c1a63f50edaf9a6ad49f3be
gnardinosaur <github email address>
2020-01-16 17:08:31 -0500

    ActionMailer test set up and working - need to redo a few models

--- app/mailers/results_mailer.rb
+++ app/mailers/results_mailer.rb
@@ -1,2 +1,9 @@
 class ResultsMailer < ApplicationMailer
+  default from: 'metartmailer@gmail.com'
+
+  def test
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Met Art Email Testing')
+  end
+
 end
