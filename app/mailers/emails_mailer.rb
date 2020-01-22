414b93496c749aa90b329f03bfd47cf3ecbd61a5
gnardinosaur <github email address>
2020-01-22 10:27:30 -0500

    stable, working app sending 1 email upon Schedule creation

--- app/mailers/emails_mailer.rb
+++ app/mailers/emails_mailer.rb
@@ -2,11 +2,11 @@ class EmailsMailer < ApplicationMailer
   default from: 'metartmailer@gmail.com'
 
   def welcome_email
-    emails.each do |email|
-      @user = email.schedule.user
-      @artwork = email.artwork
-      mail(to: @user.email, subject: 'Culture App - Art Of The Day')
-    end 
+    @user = params[:user]
+    @artwork = params[:artwork]
+    @search_info = params[:search_info]
+    @schedule_info = params[:schedule_info]
+    mail(to: @user.email, subject: 'Culture App - Art Of The Day')
   end
 
 end
